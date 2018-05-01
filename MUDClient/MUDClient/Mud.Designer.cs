bff4bdb164c85f3b0647d473ad1cc806c94b7dec
Andrew <andrewrfindlay@ntlworld.com>
2018-05-01 15:14:33 +0100

    Refactored client

---
+++ MUDClient/MUDClient/Mud.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace MUDClient
+{
+    partial class Mud
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.htmlToolTip1 = new MetroFramework.Drawing.Html.HtmlToolTip();
+            this.northTile = new MetroFramework.Controls.MetroTile();
+            this.southTile = new MetroFramework.Controls.MetroTile();
+            this.westTile = new MetroFramework.Controls.MetroTile();
+            this.eastTile = new MetroFramework.Controls.MetroTile();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.inputBox = new MetroFramework.Controls.MetroTextBox();
+            this.sendButton = new MetroFramework.Controls.MetroTile();
+            this.clientListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // htmlToolTip1
+            // 
+            this.htmlToolTip1.OwnerDraw = true;
+            // 
+            // northTile
+            // 
+            this.northTile.ActiveControl = null;
+            this.northTile.Location = new System.Drawing.Point(577, 364);
+            this.northTile.Name = "northTile";
+            this.northTile.Size = new System.Drawing.Size(76, 41);
+            this.northTile.TabIndex = 0;
+            this.northTile.Text = "North";
+            this.northTile.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.northTile.UseSelectable = true;
+            this.northTile.Click += new System.EventHandler(this.northTile_Click);
+            // 
+            // southTile
+            // 
+            this.southTile.ActiveControl = null;
+            this.southTile.Location = new System.Drawing.Point(577, 458);
+            this.southTile.Name = "southTile";
+            this.southTile.Size = new System.Drawing.Size(76, 41);
+            this.southTile.TabIndex = 0;
+            this.southTile.Text = "South";
+            this.southTile.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.southTile.UseSelectable = true;
+            this.southTile.Click += new System.EventHandler(this.southTile_Click);
+            // 
+            // westTile
+            // 
+            this.westTile.ActiveControl = null;
+            this.westTile.Location = new System.Drawing.Point(535, 411);
+            this.westTile.Name = "westTile";
+            this.westTile.Size = new System.Drawing.Size(76, 41);
+            this.westTile.TabIndex = 0;
+            this.westTile.Text = "West";
+            this.westTile.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.westTile.UseSelectable = true;
+            this.westTile.Click += new System.EventHandler(this.westTile_Click);
+            // 
+            // eastTile
+            // 
+            this.eastTile.ActiveControl = null;
+            this.eastTile.Location = new System.Drawing.Point(617, 411);
+            this.eastTile.Name = "eastTile";
+            this.eastTile.Size = new System.Drawing.Size(76, 41);
+            this.eastTile.TabIndex = 0;
+            this.eastTile.Text = "East";
+            this.eastTile.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.eastTile.UseSelectable = true;
+            this.eastTile.Click += new System.EventHandler(this.eastTile_Click);
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(23, 63);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.Size = new System.Drawing.Size(485, 342);
+            this.outputTextBox.TabIndex = 2;
+            // 
+            // inputBox
+            // 
+            // 
+            // 
+            // 
+            this.inputBox.CustomButton.Image = null;
+            this.inputBox.CustomButton.Location = new System.Drawing.Point(462, 1);
+            this.inputBox.CustomButton.Name = "";
+            this.inputBox.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.inputBox.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.inputBox.CustomButton.TabIndex = 1;
+            this.inputBox.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.inputBox.CustomButton.UseSelectable = true;
+            this.inputBox.CustomButton.Visible = false;
+            this.inputBox.Lines = new string[0];
+            this.inputBox.Location = new System.Drawing.Point(24, 428);
+            this.inputBox.MaxLength = 32767;
+            this.inputBox.Name = "inputBox";
+            this.inputBox.PasswordChar = '\0';
+            this.inputBox.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.inputBox.SelectedText = "";
+            this.inputBox.SelectionLength = 0;
+            this.inputBox.SelectionStart = 0;
+            this.inputBox.ShortcutsEnabled = true;
+            this.inputBox.Size = new System.Drawing.Size(484, 23);
+            this.inputBox.TabIndex = 3;
+            this.inputBox.UseSelectable = true;
+            this.inputBox.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.inputBox.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // sendButton
+            // 
+            this.sendButton.ActiveControl = null;
+            this.sendButton.Location = new System.Drawing.Point(24, 458);
+            this.sendButton.Margin = new System.Windows.Forms.Padding(0);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(484, 47);
+            this.sendButton.TabIndex = 4;
+            this.sendButton.Text = "Send";
+            this.sendButton.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.sendButton.UseSelectable = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // clientListBox
+            // 
+            this.clientListBox.FormattingEnabled = true;
+            this.clientListBox.Location = new System.Drawing.Point(535, 63);
+            this.clientListBox.Name = "clientListBox";
+            this.clientListBox.Size = new System.Drawing.Size(158, 277);
+            this.clientListBox.TabIndex = 5;
+            this.clientListBox.SelectedIndexChanged += new System.EventHandler(this.clientListBox_SelectedIndexChanged);
+            // 
+            // Mud
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 525);
+            this.Controls.Add(this.clientListBox);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.southTile);
+            this.Controls.Add(this.eastTile);
+            this.Controls.Add(this.westTile);
+            this.Controls.Add(this.northTile);
+            this.Name = "Mud";
+            this.Text = "MUD";
+            this.Load += new System.EventHandler(this.Mud_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Drawing.Html.HtmlToolTip htmlToolTip1;
+        private MetroFramework.Controls.MetroTile northTile;
+        private MetroFramework.Controls.MetroTile southTile;
+        private MetroFramework.Controls.MetroTile westTile;
+        private MetroFramework.Controls.MetroTile eastTile;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private MetroFramework.Controls.MetroTextBox inputBox;
+        private MetroFramework.Controls.MetroTile sendButton;
+        private System.Windows.Forms.ListBox clientListBox;
+    }
+}
+
---
+++ MUDClient/MUDClient/Mud.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Networking stuff for sockets
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+
+using MessageTypes;
+
+namespace MUDClient
+{
+    public partial class Mud : MetroFramework.Forms.MetroForm
+    {
+        // Initialise variables for networking
+        Socket client;
+        private Thread myThread;
+        bool bQuit = false;
+        bool bConnected = false;
+
+        List<String> currentClientList = new List<String>();
+
+        // Connect cilent to server
+        static void clientProcess(Object o)
+        {
+
+            Mud form = (Mud)o;
+
+            while ((form.bConnected == false) && (form.bQuit == false))
+            {
+                try
+                {
+                    form.client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    // Server IP 138.68.161.95
+                    form.client.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8500));
+                    form.bConnected = true;
+                    form.AddText("Connected to server");
+
+                    Thread receiveThread;
+
+                    receiveThread = new Thread(clientReceive);
+                    receiveThread.Start(o);
+
+                    while ((form.bQuit == false) && (form.bConnected == true))
+                    {
+                        if (form.IsDisposed == true)
+                        {
+                            form.bQuit = true;
+                            form.client.Close();
+                        }
+                    }
+
+                    receiveThread.Abort();
+                }
+                catch (System.Exception)
+                {
+                    form.AddText("No server!");
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+
+        static void clientReceive(Object o)
+        {
+            Mud form = (Mud)o;
+
+            while (form.bConnected == true)
+            {
+                try
+                {
+                    byte[] buffer = new byte[4096];
+                    int result;
+
+                    result = form.client.Receive(buffer);
+
+                    if (result > 0)
+                    {
+                        MemoryStream stream = new MemoryStream(buffer);
+                        BinaryReader read = new BinaryReader(stream);
+
+                        Msg m = Msg.DecodeStream(read);
+
+                        if (m != null)
+                        {
+                            Console.Write("Got a message: ");
+                            switch (m.mID)
+                            {
+                                case PublicChatMsg.ID:
+                                    {
+                                        PublicChatMsg publicMsg = (PublicChatMsg)m;
+
+                                        form.AddText(publicMsg.msg);
+                                    }
+                                    break;
+
+                                case PrivateChatMsg.ID:
+                                    {
+                                        PrivateChatMsg privateMsg = (PrivateChatMsg)m;
+                                        form.AddText(privateMsg.msg);
+                                    }
+                                    break;
+
+                                case ClientListMsg.ID:
+                                    {
+                                        ClientListMsg clientList = (ClientListMsg)m;
+
+                                        form.SetClientList(clientList);
+                                    }
+                                    break;
+
+                                case ClientNameMsg.ID:
+                                    {
+                                        ClientNameMsg clientName = (ClientNameMsg)m;
+
+                                        form.SetClientName(clientName.name);
+                                    }
+                                    break;
+
+                                default:
+                                    break;
+                            }
+                        }
+                    }
+
+                }
+                catch (Exception)
+                {
+                    form.bConnected = false;
+                    Console.WriteLine("Lost server!");
+                }
+
+            }
+        }
+
+        // Initialisation
+        public Mud()
+        {
+            InitializeComponent();
+
+            myThread = new Thread(clientProcess);
+            myThread.Start(this);
+
+            Application.ApplicationExit += delegate { OnExit(); };
+        }
+
+        private void Mud_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // WINFORM FUNCTIONS {
+        // NAVIGATION {
+        // Send north direction
+        private void northTile_Click(object sender, EventArgs e)
+        {
+            // Create navigation message
+            createNavigationMessageFromString("go north");
+        }
+
+        // Send east direction
+        private void eastTile_Click(object sender, EventArgs e)
+        {
+            // Create navigation message
+            createNavigationMessageFromString("go east");
+        }
+
+        // Send south direction
+        private void southTile_Click(object sender, EventArgs e)
+        {
+            // Create navigation message
+            createNavigationMessageFromString("go south");
+        }
+
+        // Send west direction
+        private void westTile_Click(object sender, EventArgs e)
+        {
+            // Create navigation message
+            createNavigationMessageFromString("go west");
+        }
+
+        // Sends navigation message to server
+        private void createNavigationMessageFromString(String direction)
+        {
+            // Check for server connection
+            if(bConnected)
+            {
+                // Create navigation message
+                DungeonNavigationMsg navigationMsg = new DungeonNavigationMsg();
+                navigationMsg.msg = direction;
+
+                MemoryStream outStream = navigationMsg.WriteData();
+                client.Send(outStream.GetBuffer());
+            }
+            else
+            {
+                AddText("No server, navigation disabled.");
+            }
+        }
+        // } NAVIGATION
+
+        private delegate void AddTextDelegate(String s);
+        
+        // Add to main output text box
+        private void AddText(String s)
+        {
+            if (outputTextBox.InvokeRequired)
+            {
+                Invoke(new AddTextDelegate(AddText), new object[] { s });
+            }
+            else
+            {
+                outputTextBox.Text += s;
+                outputTextBox.Text += Environment.NewLine;
+            }
+        }
+
+        private delegate void SetClientNameDelegate(String s);
+        private void SetClientName(String s)
+        {
+            if (this.InvokeRequired)
+            {
+                Invoke(new SetClientNameDelegate(SetClientName), new object[] { s });
+            }
+            else
+            {
+                Text = s;
+            }
+        }
+
+        private delegate void SetClientListDelegate(ClientListMsg clientList);
+        private void SetClientList(ClientListMsg clientList)
+        {
+            if (this.InvokeRequired)
+            {
+                Invoke(new SetClientListDelegate(SetClientList), new object[] { clientList });
+            }
+            else
+            {
+                clientListBox.DataSource = null;
+                currentClientList.Clear();
+                currentClientList.Add("Room");
+
+                foreach (String s in clientList.clientList)
+                {
+                    currentClientList.Add(s);
+                }
+                clientListBox.DataSource = currentClientList;
+            }
+        }
+
+        // } WINFORM FUNCTIONS
+
+        // Send text input
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            if ((inputBox.Text.Length > 0) && (client != null))
+            {
+                try
+                {
+                    if (clientListBox.SelectedIndex == 0)
+                    {
+                        // Create public message
+                        PublicChatMsg publicMsg = new PublicChatMsg();
+
+                        publicMsg.msg = inputBox.Text;
+                        MemoryStream outStream = publicMsg.WriteData();
+                        client.Send(outStream.GetBuffer());
+                    }
+                    else
+                    {
+                        // Create private message
+                        PrivateChatMsg privateMsg = new PrivateChatMsg();
+
+                        privateMsg.msg = inputBox.Text;
+                        privateMsg.destination = currentClientList[clientListBox.SelectedIndex];
+                        MemoryStream outStream = privateMsg.WriteData();
+                        client.Send(outStream.GetBuffer());
+                    }
+
+                }
+                catch (System.Exception)
+                {
+                }
+
+                inputBox.Text = "";
+            }
+        }
+
+        private void OnExit()
+        {
+            bQuit = true;
+            Thread.Sleep(500);
+            if (myThread != null)
+            {
+                myThread.Abort();
+            }
+        }
+
+        private void clientListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
